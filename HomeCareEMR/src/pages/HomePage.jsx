55bcb437153e4d50f407811a9bafc991ffba23b7
JerichoValerio <jerichotvalerio@gmail.com>
2023-10-12 20:45:09 -0700

    added footer

--- HomeCareEMR/src/pages/HomePage.jsx
+++ HomeCareEMR/src/pages/HomePage.jsx
@@ -30,9 +30,26 @@ const gridItemStyle = {
   padding: '20px',
   border: '1px solid gray',
   borderRadius: '5px',
+  transition: 'background-color 0.3s', // Smooth transition for hover effect
+};
+
+const gridItemHoverStyle = {
+  backgroundColor: 'lightcoral', // New background color on hover
+};
+
+const footerStyle = {
+  position: 'fixed',
+  bottom: 0,
+  left: 0,
+  right: 0,
+  backgroundColor: 'lightgray',
+  padding: '10px',
+  textAlign: 'center',
 };
 
 export default function HomePage() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div>
       <nav style={navStyle}>
@@ -48,16 +65,18 @@ export default function HomePage() {
       </nav>
       <div style={{ marginTop: '60px' }}>
         <div style={gridContainerStyle}>
-          <div style={gridItemStyle}>Item 1</div>
-          <div style={gridItemStyle}>Item 2</div>
-          <div style={gridItemStyle}>Item 3</div>
-          <div style={gridItemStyle}>Item 4</div>
-          <div style={gridItemStyle}>Item 5</div>
-          <div style={gridItemStyle}>Item 6</div>
+          <div style={{ ...gridItemStyle, ':hover': gridItemHoverStyle }}>Item 1</div>
+          <div style={{ ...gridItemStyle, ':hover': gridItemHoverStyle }}>Item 2</div>
+          <div style={{ ...gridItemStyle, ':hover': gridItemHoverStyle }}>Item 3</div>
+          <div style={{ ...gridItemStyle, ':hover': gridItemHoverStyle }}>Item 4</div>
+          <div style={{ ...gridItemStyle, ':hover': gridItemHoverStyle }}>Item 5</div>
+          <div style={{ ...gridItemStyle, ':hover': gridItemHoverStyle }}>Item 6</div>
         </div>
         care
       </div>
+      <footer style={footerStyle}>
+        &copy; {currentYear} HomeCare EMR
+      </footer>
     </div>
   );
 }
-
