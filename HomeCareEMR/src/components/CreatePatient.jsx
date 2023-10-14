e06a7c3bdac6150b7fab53f7837ed56f9ead4ecb
JerichoValerio <jerichotvalerio@gmail.com>
2023-10-14 10:51:38 -0700

    mark deleted my code

--- HomeCareEMR/src/components/CreatePatient.jsx
+++ HomeCareEMR/src/components/CreatePatient.jsx
@@ -34,7 +34,63 @@ export default function CreatePatient() {
     const [anticoagulantName, setAnticoagulantName] = useState('')
     const [anticoagulantDose, setAnticoagulantDose] = useState('')
 
-
+    const navStyle = {
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: '#0055A4', // Professional blue color
+        color: 'yellow', // White text for better contrast
+        padding: '10px',
+        display: 'flex',
+        justifyContent: 'center',
+      };
+      const buttonStyle = {
+        margin: '5px',
+        padding: '10px 20px',
+        backgroundColor: 'white', // Light blue background
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer',
+        color: 'yellow', // White text
+        fontWeight: 'bold', // Bold text
+        textDecoration: 'none',
+      };
+      const gridItemStyle = {
+        backgroundColor: 'white',
+        padding: '20px',
+        border: '1px solid #E0E0E0', // Light gray border
+        borderRadius: '5px',
+        transition: 'background-color 0.3s',
+        boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)', // Subtle box shadow
+      };
+      const gridItemHoverStyle = {
+        backgroundColor: '#F0F0F0', // Light gray background on hover
+      };
+      const footerStyle = {
+        position: 'fixed',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: '#0055A4', // Match the top navigation bar
+        color: 'white',
+        padding: '10px',
+        textAlign: 'center',
+      };
+      const formContainerStyle = {
+        marginTop: '80px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      };
+      
+      const marqueeStyle = {
+          backgroundColor: 'red',
+          color: 'white',
+          overflow: 'hidden',
+          whiteSpace: 'nowrap',
+          animation: 'marquee 30s linear infinite', // Adjust the duration to 30 seconds
+        };
 
     const handleGender = (event) => {
         setGender(event.target.value);
@@ -137,6 +193,22 @@ export default function CreatePatient() {
 
   return (
     <>
+     <marquee behavior="scroll" direction="left" style={{ backgroundColor: 'red', color: 'white' }}>
+      This page is off-limits to the public as it is a company pipeline tool, and it is for admin access only; unauthorized access will result in consequences.
+      </marquee>
+
+      <nav style={navStyle}>
+        <button style={buttonStyle}>
+          <a href="/">Home</a>
+        </button>
+        <button style={buttonStyle}>
+          <a href="/about">About</a>
+        </button>
+        <button style={buttonStyle}>
+          <a href="/contact">Contact</a>
+        </button>
+      </nav>
+
         <Container >
             <Typography variant="h5">Create Patient</Typography>
             <form onSubmit={handleSubmit} action={<Link to="/" />}>
@@ -738,7 +810,6 @@ export default function CreatePatient() {
                 <Button 
                 variant="outlined" color="secondary" type="submit">Submit</Button>
                 
-
             </form>
         </Container>
     </>
