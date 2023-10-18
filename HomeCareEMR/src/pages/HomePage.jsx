294d09d5adb40f5adfa1f3e9bf3b73cd271bc7bb
JerichoValerio <jerichotvalerio@gmail.com>
2023-10-18 16:03:29 -0700

    working on side bar

--- HomeCareEMR/src/pages/HomePage.jsx
+++ HomeCareEMR/src/pages/HomePage.jsx
@@ -17,28 +17,44 @@ export default function HomePage() {
           <a href="/contact">Contact</a>
         </button>
       </nav>
-      <div className="header">
-        <h1>Welcome to HomeCare EMR</h1>
-        <p>Your Trusted Partner in Healthcare ♥️</p>
-      </div>
-      <div style={{ marginTop: '20px' }}>
-        <div className="grid-container">
-          <div className="grid-item">
-            <h2>Services</h2>
-            <p>We offer a wide range of medical services to cater to your needs.</p>
-          </div>
-          <div className="grid-item">
-            <h2>Doctors</h2>
-            <p>Meet our experienced and dedicated medical professionals.</p>
-          </div>
-          <div className="grid-item">
-            <h2>Appointments</h2>
-            <p>Schedule an appointment and receive quality healthcare.</p>
-          </div>
+      <div className="content">
+        <div className="sidebar">
+          <h3>Sidebar</h3>
+          <ul>
+            <li>
+              <a href="/services">Services</a>
+            </li>
+            <li>
+              <a href="/doctors">Doctors</a>
+            </li>
+            <li>
+              <a href="/appointments">Appointments</a>
+            </li>
+          </ul>
         </div>
-        <p style={{ textAlign: 'center', marginTop: '20px' }}>
-          We are committed to your well-being.
-        </p>
+        <div className="main-content">
+          <div className="header">
+            <h1>Welcome to HomeCare EMR</h1>
+            <p>Your Trusted Partner in Healthcare ♥️</p>
+          </div>
+          <div className="grid-container">
+            <div className="grid-item">
+              <h2>Services</h2>
+              <p>We offer a wide range of medical services to cater to your needs.</p>
+            </div>
+            <div className="grid-item">
+              <h2>Doctors</h2>
+              <p>Meet our experienced and dedicated medical professionals.</p>
+            </div>
+            <div className="grid-item">
+              <h2>Appointments</h2>
+              <p>Schedule an appointment and receive quality healthcare.</p>
+            </div>
+          </div>
+          <p style={{ textAlign: 'center', marginTop: '20px' }}>
+            We are committed to your well-being.
+          </p>
+        </div>
       </div>
       <footer className="footer">
         &copy; {currentYear} HomeCare EMR
