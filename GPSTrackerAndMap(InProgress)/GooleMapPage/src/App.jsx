c9eac7ff0f6b0d77bff4d095aef858c680dd67f7
Bill Kuo <billkuo08@gmail.com>
2023-10-20 21:19:50 -0700

    Routes: DirectionsService & DirectionsRender, problem:cant zoom in or out

--- GPSTrackerAndMap(InProgress)/GooleMapPage/src/App.jsx
+++ GPSTrackerAndMap(InProgress)/GooleMapPage/src/App.jsx
@@ -3,15 +3,18 @@ import {
   useJsApiLoader,
   GoogleMap,
   Marker,
+  Polyline
 } from "@react-google-maps/api";
 import { getDatabase, ref, onValue } from "firebase/database";
 import db from "./firebase_setup/firebase";
 import car from './car.png';
+import { DirectionsService } from "@react-google-maps/api";
+import { DirectionsRenderer } from "@react-google-maps/api";
 
 
 const center = {
-  lat: 49.3347735696573,
-  lng: -123.15848589440924,
+  lat: 49.33473336980647,
+  lng: -123.15846009191421,
 };
 
 function App() {
@@ -24,9 +27,10 @@ function App() {
   });
 
   const [currentLocation, setCurrentLocation] = useState([]);
+  const [directions, setDirections] = useState(null); // Add this state for DirectionsRenderer
 
   const locations = [
-    { lat: 49.28156434232159, lng: -123.111261173516 }, // First set of coordinates
+    { lat: 49.33473336980647, lng: -123.15846009191421 }, // First set of coordinates
     { lat: 49.27679087948593, lng: -123.11163668254494 }, // Second set of coordinates
   ];
 
@@ -39,37 +43,48 @@ function App() {
     });
   };
 
-  
-
   useEffect(() => {
     fetchdata();
-    if (isLoaded && google) {
-      const directionsService = new google.maps.DirectionsService();
 
-      const origin = new google.maps.LatLng(49.28156434232159, -123.111261173516);
-      const destination = new google.maps.LatLng(49.27679087948593, -123.11163668254494);
+        // if (isLoaded && google) {
+    //   const directionsService = new google.maps.DirectionsService();
 
-      const request = {
-        origin: origin,
-        destination: destination,
-        travelMode: 'DRIVING', // You can change the travel mode as needed
-      };
+    //   const origin = new google.maps.LatLng(49.28156434232159, -123.111261173516);
+    //   const destination = new google.maps.LatLng(49.27679087948593, -123.11163668254494);
 
-      directionsService.route(request, (result, status) => {
-        if (status === 'OK') {
-          // Handle the result and extract the path of the route
-          const route = result.routes[0];
-          // Display the route on your map using the Polyline component.
-        }
-      });
-    }
+    //   const request = {
+    //     origin: origin,
+    //     destination: destination,
+    //     travelMode: 'DRIVING', // You can change the travel mode as needed
+    //   };
+
+    //   directionsService.route(request, (result, status) => {
+    //     if (status === 'OK') {
+    //       // Handle the result and extract the path of the route
+    //       const route = result.routes[0];
+    //       // Display the route on your map using the Polyline component.
+    //     }
+    //   });
+    // }
   }, []);
+
+  const directionsCallback = (response) => {
+    if (response !== null) {
+      if (response.status === 'OK') {
+        setDirections(response);
+      } else {
+        console.error('Directions request failed with status:', response.status);
+      }
+    } else {
+      console.error('Directions request failed. Response is null.');
+    }
+  };
 
   return isLoaded ? (
     <>
       <GoogleMap
         center={center}
-        zoom={8}
+        zoom={12}
         mapContainerStyle={{ width: "100%", height: "100vh" }}
         options={{
           zoomControl: false,
@@ -78,8 +93,17 @@ function App() {
           fullscreenControl: false,
         }}
       >
-        <Marker position={currentLocation} icon={car}>
-        </Marker>
+        {/* <Marker position={currentLocation} icon={car}>
+        </Marker> */}
+        <Marker
+          position={
+            typeof currentLocation.lat === 'number' && typeof currentLocation.lng === 'number'
+              ? currentLocation
+              : null // Set a default position or null if currentLocation is invalid
+          }
+          icon={car}
+        />
+
 
         {locations.map((location, index) => (
           <Marker
@@ -92,13 +116,31 @@ function App() {
           />
         ))}
 
+        {currentLocation.lat && currentLocation.lng && locations[0] && locations[1] ? (
+          <DirectionsService
+            options={{
+              destination: locations[0], // Destination marker coordinates
+              origin: locations[1], // Current location marker coordinates
+              travelMode: "DRIVING", // You can change this to your preferred travel mode
+            }}
+            callback={directionsCallback}
+          />
+        ) : null}
+
+        {directions && (
+          <DirectionsRenderer
+            directions={directions}
+            options={{
+              suppressMarkers: true,
+            }}
+          />
+        )}
 
       </GoogleMap>
     </>
-  ) : (
-    <></>
-  );
-  
+  ) : null;
+
+
 }
 
 export default App
