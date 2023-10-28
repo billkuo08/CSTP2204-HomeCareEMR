2c90b63f0aba11a9ec05b48ddbc7334905ede05d
Tsung-Chih Ke <000447172@student.vcc.ca>
2023-10-28 13:37:24 -0700

    edit patient list file

--- HomeCareEMR/src/components/EditPatient.jsx
+++ HomeCareEMR/src/components/EditPatient.jsx
@@ -193,7 +193,7 @@ export default function CreatePatient() {
       </marquee>
 
       <Container className="form-container" >
-        <Typography variant="h5" className="h5"><b><CreateNewFolderTwoToneIcon /> Create Patient <CreateTwoToneIcon /></b></Typography>
+        <Typography variant="h5" className="h5"><b><CreateNewFolderTwoToneIcon /> Edit Patient <CreateTwoToneIcon /></b></Typography>
         <form onSubmit={handleSubmit} action={<Link to="/" />} >
           <Stack spacing={1} direction="row" sx={{ marginBottom: 0, marginTop: 1 }}>
             <TextField
