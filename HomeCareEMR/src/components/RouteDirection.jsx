22852c3f9f01814e56f320721247f4e425a2fcfe
Bill Kuo <billkuo08@gmail.com>
2023-11-03 20:05:01 -0700

    directions button and DirectionsService changes

--- HomeCareEMR/src/components/RouteDirection.jsx
+++ HomeCareEMR/src/components/RouteDirection.jsx
@@ -5,7 +5,7 @@ import {
     GoogleMap,
     Marker,
 } from "@react-google-maps/api";
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getDatabase, ref, onValue, set } from "firebase/database";
 import { dbMap, mapAPIKey } from "../config/config"
 import car from '../images/car.png';
 import { DirectionsService } from "@react-google-maps/api";
@@ -30,13 +30,16 @@ function RouteDirection() {
     });
 
     const [currentLocation, setCurrentLocation] = useState([]);
-    const [directions, setDirections] = useState(null); // Add this state for DirectionsRenderer
+    const [directions, setDirections] = useState(null); // Store directions in state
     const [patientData, setPatientData] = useState();
+    const [patientsLocationData, setPatientLocation] = useState([]);
 
-    const locations = [
-        { lat: 49.33473336980647, lng: -123.15846009191421 }, // First set of coordinates
-        { lat: 49.27679087948593, lng: -123.11163668254494 }, // Second set of coordinates
-    ];
+
+
+    // const myLocations = [
+    //     { lat: 49.33473336980647, lng: -123.15846009191421 }, // First set of coordinates
+    //     { lat: 49.27679087948593, lng: -123.11163668254494 }, // Second set of coordinates
+    // ];
 
     const fetchdata = async () => {
 
@@ -44,8 +47,20 @@ function RouteDirection() {
         onValue(starCountRef, (snapshot) => {
             const data = snapshot.val();
             setCurrentLocation(data);
-            console.log(data);
         });
+
+        //Fetch "location" data
+        const patientsLocationCollectionRef = collection(firestoredb, 'patients');
+        const patientsLocationCollectionSnapshot = await getDocs(patientsLocationCollectionRef);
+        const patientsLocation = patientsLocationCollectionSnapshot.docs.map((doc) => doc.data());
+        const latlngPatientLocationData = patientsLocation.filter((data) => data.lat !== undefined && data.lng !== undefined).map((data) => ({
+            id: data.id,
+            lat: Number(data.lat),
+            lng: Number(data.lng)
+        }));
+
+        setPatientLocation(latlngPatientLocationData);
+        console.log(latlngPatientLocationData)
 
         // Fetch "patients" collection data
         const patientsCollectionRef = collection(firestoredb, 'patients');
@@ -53,17 +68,12 @@ function RouteDirection() {
         const patientsData = patientsCollectionSnapshot.docs.map((doc) => doc.data());
         setPatientData(patientsData);
 
-        // patientsData.map((patient) => {
-        //     const patientAddress = patient.address;
-        //     const firstName = patient.firstname;
-        //     const lastName = patient.lastname;
-        // });
-
-
     };
 
     useEffect(() => {
+
         fetchdata();
+
 
     }, []);
 
@@ -81,12 +91,43 @@ function RouteDirection() {
 
     };
 
+    const changeDestination = () => {
+
+        const buttonId = event.target.id;
+        const patientWithSameId = patientsLocationData.find(patient => patient.id === buttonId);
+
+        if (patientWithSameId) {
+            // Update the destination in the DirectionsService to the patient's location
+            const updatedDestination = {
+                lat: patientWithSameId.lat,
+                lng: patientWithSameId.lng,
+            };
+
+            // Create a new DirectionsService request with the updated destination
+            const directionsService = new window.google.maps.DirectionsService();
+            directionsService.route(
+                {
+                    destination: updatedDestination,
+                    origin: currentLocation,
+                    travelMode: "DRIVING", // You can change this to your preferred travel mode
+                },
+                directionsCallback
+            );
+        } else {
+            console.log(`No patient found with ID: ${buttonId}`);
+        }
+
+
+
+    };
+
+
     return isLoaded ? (
         <>
             <div className="map-container">
                 <GoogleMap
                     center={center}
-                    zoom={12}
+                    zoom={11}
                     mapContainerStyle={{
                         width: "100%",
                         height: "65vh",
@@ -108,32 +149,33 @@ function RouteDirection() {
                         icon={car}
                     />
 
+                    {patientsLocationData ?
+                        patientsLocationData.map((location, index) => (
+                            <Marker
+                                key={index}
+                                position={location}
+                                icon={{
+                                    url: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png',
+                                    scaledSize: new window.google.maps.Size(32, 32),
+                                }}
+                            />
+                        ))
+                        : null
+                    }
 
-                    {locations.map((location, index) => (
-                        <Marker
-                            key={index}
-                            position={location}
-                            icon={{
-                                url: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png', // URL to the red location icon
-                                scaledSize: new window.google.maps.Size(32, 32), // Adjust the size as needed
-                            }}
-                        />
-                    ))}
-
-                    {currentLocation.lat && currentLocation.lng && locations[0] && locations[1] ? (
+                    {/* {patientsLocationData ?
                         <DirectionsService
                             options={{
-                                destination: locations[0], // Destination marker coordinates
-                                origin: locations[1], // Current location marker coordinates
+                                destination: patientsLocationData[1], // Destination marker coordinates
+                                origin: currentLocation, // Current location marker coordinates
                                 travelMode: "DRIVING", // You can change this to your preferred travel mode
                             }}
-                            callback={directionsCallback}
                         />
-                    ) : null}
+                        : null} */}
 
                     {directions && (
                         <DirectionsRenderer
-                            directions={directions}
+                            directions={directions} // Display directions based on stored state
                             options={{
                                 preserveViewport: true,
                                 suppressMarkers: true,
@@ -152,7 +194,7 @@ function RouteDirection() {
                         <div className="patient-box" key={index}>
                             <h3>Full Name: {patient.firstName + " " + patient.lastName}</h3>
                             <h4>Address: {patient.address}</h4>
-                            <button className="btn-locate">Locate</button>
+                            <button className="btn-locate" id={patient.id} onClick={changeDestination}>Direction</button>
                         </div>
                     ))
                 ) : (
