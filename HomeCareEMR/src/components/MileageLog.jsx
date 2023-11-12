2744c2be450cff889a4bed02b3ad72e59f79ff52
Bill Kuo <billkuo08@gmail.com>
2023-11-11 22:41:47 -0800

    display mileageData on mileageLog page in a simple table form

--- HomeCareEMR/src/components/MileageLog.jsx
+++ HomeCareEMR/src/components/MileageLog.jsx
@@ -1,7 +1,45 @@
-
+import { useState, useEffect } from 'react'
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 
 function DisplayMileageLog(){
+    const [mileageLog, setMileageLog] = useState([]);
+
+    useEffect(() => {
+        const db = getFirestore();
+        const mileageLogCollection = collection(db, "mileage");
+        getDocs(mileageLogCollection).then((querySnapshot) => {
+            const mileageLogArray = querySnapshot.docs.map(doc => doc.data());
+            setMileageLog(mileageLogArray);
+            console.log(mileageLogArray)
+        });
+    }, []);
+
+    return (
+        <div>
+            <h1>Mileage Log</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Date</th>
+                        <th>Total Distance</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {mileageLog.map((mileageLog, index) => (
+                        <tr key={index}>
+                            <td>{mileageLog.FirstName}</td>
+                            <td>{mileageLog.LastName}</td>
+                            <td>{mileageLog.Date}</td>
+                            <td>{mileageLog.DistanceTraveled}</td>                           
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 
 }
 
